57eb30da470c31635c82d49434c56978433081a0
Mateusz Banasik <kilifugame@gmail.com>
2024-04-09 23:02:31 +0200

    Add Generator

---
+++ Generator/Program.cs
@@ -0,0 +1,25 @@
+﻿using LogicServer;
+using Newtonsoft.Json.Schema;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+using JsonSchema = NJsonSchema.JsonSchema;
+
+namespace Generator;
+
+internal static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("JSON Schema: \n\n");
+        JsonSchema schema = JsonSchema.FromType<IShopItem>();
+        string schemaString = schema.ToJson();
+
+        Console.WriteLine(schemaString);
+
+        Console.WriteLine("C#: \n\n");
+
+        JsonSchema loadedSchema = await JsonSchema.FromJsonAsync(schemaString);
+        CSharpGenerator csharpGenerator = new CSharpGenerator(loadedSchema);
+        Console.WriteLine(csharpGenerator.GenerateFile());
+    }
+}
